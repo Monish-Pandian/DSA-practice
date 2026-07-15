dc8b0048ea59f19c59c5cbb629f5776f37e6ef05
Monish-Pandian <monishpandian.25@gmail.com>
2026-07-15 20:04:25 +0530

    Add Problem :3658_GCDOfOddAndEvenSums.java

---
+++ math/3658_GCDOfOddAndEvenSums.java
@@ -0,0 +1,14 @@
+/**
+ * Problem: GCD of Odd and Even Sums (#3658)
+ * Difficulty: Easy
+ * Pattern: Math, GCD
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ * LeetCode: https://leetcode.com/problems/gcd-of-odd-and-even-sums/
+ */
+
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+       return n;
+    }
+}
