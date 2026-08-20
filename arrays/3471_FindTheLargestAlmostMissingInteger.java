2aead9b72d2c684035beb5d2f461f48767de6c12
Monish-Pandian <monishpandian.25@gmail.com>
2026-08-20 20:34:00 +0530

    3471_FindTheLargestAlmostMissingInteger.java

---
+++ arrays/3471_FindTheLargestAlmostMissingInteger.java
@@ -0,0 +1,42 @@
+/**
+ * Problem: Largest Integer With Maximum K Occurrences (#? )
+ * Difficulty: Medium
+ * Pattern: Array, Frequency Counting
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * LeetCode: https://leetcode.com/
+ */
+
+class Solution {
+
+    public int largestInteger(int[] nums, int k) {
+        int n = nums.length;
+        if (n == k) {
+            int res = nums[0];
+            for (int x : nums) {
+                res = Math.max(res, x);
+            }
+            return res;
+        }
+        int[] count = new int[51];
+        for (int x : nums) {
+            count[x]++;
+        }
+        if (k == 1) {
+            for (int i = 50; i >= 0; --i) {
+                if (count[i] == 1) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        int res = -1;
+        if (count[nums[0]] == 1) {
+            res = Math.max(res, nums[0]);
+        }
+        if (count[nums[n - 1]] == 1) {
+            res = Math.max(res, nums[n - 1]);
+        }
+        return res;
+    }
+}
