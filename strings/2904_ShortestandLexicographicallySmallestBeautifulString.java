db7b85e656609792d7b072898363e1c34e2770d1
Monish-Pandian <monishpandian.25@gmail.com>
2026-08-26 20:19:58 +0530

    Add Problem:2904_ShortestandLexicographicallySmallestBeautifulString.java

---
+++ strings/2904_ShortestandLexicographicallySmallestBeautifulString.java
@@ -0,0 +1,37 @@
+/**
+ * Problem: Shortest Beautiful Substring (#2904)
+ * Difficulty: Medium
+ * Pattern: Brute Force, String, Sliding Window Concept
+ * Time Complexity: O(n³)
+ * Space Complexity: O(n)
+ * LeetCode: https://leetcode.com/problems/shortest-and-beautiful-substring/
+ */
+class Solution {
+
+    public String shortestBeautifulSubstring(String s, int k) {
+        int n = s.length();
+
+        for (int m = k; m <= n; m++) {
+            String ans = "";
+
+            for (int i = m; i <= n; i++) {
+                String t = s.substring(i - m, i);
+                int cnt = 0;
+
+                for (int j = 0; j < t.length(); j++) {
+                    cnt += t.charAt(j) - '0';
+                }
+
+                if ((ans.isEmpty() || t.compareTo(ans) < 0) && cnt == k) {
+                    ans = t;
+                }
+            }
+
+            if (!ans.isEmpty()) {
+                return ans;
+            }
+        }
+
+        return "";
+    }
+}
