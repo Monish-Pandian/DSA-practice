6ddf7c0c422a84a0d71318a2d08c7e55ce9d2896
Monish-Pandian <monishpandian.25@gmail.com>
2026-09-09 21:25:28 +0530

    Add Problem:3875_ConstructUniformParityArrayI.java

---
+++ arrays/3875_ConstructUniformParityArrayI.java
@@ -0,0 +1,14 @@
+/**
+ * Problem: Construct Uniform Parity Array I
+ * Difficulty: Easy
+ * Pattern: Array, Math, Parity
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ * LeetCode: https://leetcode.com/problems/construct-uniform-parity-array-i/
+ */
+
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
